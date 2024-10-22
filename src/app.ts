1a5c3be8b097bb7c71026f4145357dd2265514dc
Kuldeep Rathod <kuldiprathod375@gmail.com>
2024-10-22 20:59:07 +0530

    added multer

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import dotenv from "dotenv";
 
 //importing routes
 import userRoutes from "./routes/user.js";
@@ -6,7 +7,9 @@ import { connect } from "http2";
 import { connectDB } from "./utils/features.js";
 import { errorMiddleware } from "./middlewares/error.js";
 
-const port = 4000;
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
